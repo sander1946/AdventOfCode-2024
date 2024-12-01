7949002b4255624b8a514443a85efbf6629a77fc
sander1946 <71840186+sander1946@users.noreply.github.com>
2024-12-01 23:54:53 +0100

    implemented these algorithms

--- src/main/java/com/advent/of/code/Main.java
+++ src/main/java/com/advent/of/code/Main.java
@@ -1,7 +1,5 @@
 package com.advent.of.code;
 
-import java.util.ArrayList;
-
 public class Main {
     public static void dayOne() {
         // Part One
@@ -9,67 +7,61 @@ public class Main {
         Scraper scraper = new Scraper();
         String input = scraper.getInput();
         String[] splitInput = input.split("\n");
-        ArrayList<Integer> left = new ArrayList<>();
-        ArrayList<Integer> right = new ArrayList<>();
+        MinHeap heapOne = new MinHeap();
+        MinHeap heapTwo = new MinHeap();
 
         for (String splitInput1 : splitInput) {
             String[] split = splitInput1.split("   ");
             split[0] = split[0].strip();
             split[1] = split[1].strip();
-            left.add(Integer.valueOf(split[0]));
-            right.add(Integer.valueOf(split[1]));
+            heapOne.push(Integer.valueOf(split[0]));
+            heapTwo.push(Integer.valueOf(split[1]));
         }
 
-        if (left.size() != right.size()) {
+        if (heapOne.size() != heapTwo.size()) {
             System.out.println("The number of left and right values do not match");
             return;
         }
 
-        while (left.size() >= 1){
-            int leftSmallest = left.get(0);
-            int rightSmallest = right.get(0);
-
-            for (int i = 0; i < left.size(); i++) {
-                if (left.get(i) < leftSmallest) {
-                    leftSmallest = left.get(i);
-                }
-                if (right.get(i) < rightSmallest) {
-                    rightSmallest = right.get(i);
-                }
-            }
-            left.remove(left.indexOf(leftSmallest));
-            right.remove(right.indexOf(rightSmallest));
+        while (heapOne.size() >= 1) {
+            int leftSmallest = heapOne.pop();
+            int rightSmallest = heapTwo.pop();
             int distance = Math.abs(leftSmallest - rightSmallest);
             totalDistance += distance;
         }
-        System.out.println("The total distance is: " + totalDistance);
+        System.out.println("The total distance is:    " + totalDistance);
 
         // Part Two
-        int totalSimulairity = 0;
+        int totalSimilarity = 0;
 
-        // Add the values to the left and right arraylist again
+        MinHeap heap = new MinHeap();
+        BinaryTree tree = new BinaryTree();
+
         for (String splitInput1 : splitInput) {
             String[] split = splitInput1.split("   ");
             split[0] = split[0].strip();
             split[1] = split[1].strip();
-            left.add(Integer.valueOf(split[0]));
-            right.add(Integer.valueOf(split[1]));
+            heap.push(Integer.valueOf(split[0]));
+            tree.insert(Integer.parseInt(split[1]));
         }
 
-        for (int leftInt : left) {
-            int rightCounter = 0;
-            for (int rightInt : right) {
-                if (rightInt == leftInt) {
-                    rightCounter++;
-                }
+        while (heap.size() >= 1) {
+            int leftSmallest = heap.pop();
+            Node node = tree.search(leftSmallest);
+            if (node != null) {
+                totalSimilarity += leftSmallest * node.count;
             }
-            int simulairity = leftInt * rightCounter;
-            totalSimulairity += simulairity;
         }
-        System.out.println("The total simulairity is: " + totalSimulairity);
+        System.out.println("The total simulairity is: " + totalSimilarity);
     }
 
     public static void main(String[] args) {
+        System.out.println("Advent of Code 2024");
+        System.out.println("Day One");
+        long startTime = System.nanoTime();
         dayOne();
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime);  //divide by 1000000 to get milliseconds
+        System.out.println("Execution time: " + duration / 1000000 + " milliseconds");
     }
 }
