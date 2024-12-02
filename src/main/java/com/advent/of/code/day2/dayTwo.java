d8a68a94d3e1e5e4af232e997b68d335449c0d66
sander1946 <71840186+sander1946@users.noreply.github.com>
2024-12-02 22:50:50 +0100

    update the layout of day two

--- src/main/java/com/advent/of/code/day2/dayTwo.java
+++ src/main/java/com/advent/of/code/day2/dayTwo.java
@@ -7,6 +7,7 @@ import com.advent.of.code.scraper.Scraper;
 public class dayTwo {
     public static void run() {
         System.out.println("\n===== Day Two =============");
+        // Part One
         Scraper scraper = new Scraper(2);
         String[] input = scraper.getInput();
         ArrayList<Raport> raportList = new ArrayList<>();
@@ -26,14 +27,15 @@ public class dayTwo {
                 totalOne++;
             }
         }
+        System.out.println("The total number of safe raports with no removal is:  " + totalOne);
 
+        // Part Two
         int totalTwo = 0;
         for (Raport raport : raportList) {
             if (raport.isSaveWithOneRemoval()) {
                 totalTwo++;
             }
         }
-        System.out.println("The total number of safe raports with no removal is:  " + totalOne);
         System.out.println("The total number of safe raports with one removal is: " + totalTwo);
     }
 }
