f9eb145ecbc997d2e3fd37497bd5f3812a929e89
sander1946 <71840186+sander1946@users.noreply.github.com>
2024-12-03 10:53:04 +0100

    day three part one

--- src/main/java/com/advent/of/code/day3/dayThree.java
+++ src/main/java/com/advent/of/code/day3/dayThree.java
@@ -1,14 +1,37 @@
 package com.advent.of.code.day3;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.advent.of.code.scraper.Scraper;
 
 /**
  * Day Three solution
  */
 public class dayThree {
+    private static boolean dont = false;
     /**
      * Run the solution for day three
      */
     public static void run() {
         System.out.println("\n===== Day Three ===========");
+        // Part One
+        Scraper scraper = new Scraper(3);
+        String[] input = scraper.getInput();
+        int total = 0;
+        for (String line : input) {
+            Pattern pattern = Pattern.compile("mul\\([0-9]+,[0-9]+\\)");
+            Matcher matcher = pattern.matcher(line);
+            while (matcher.find()) {
+                int leftDigit = Integer.parseInt(matcher.group().split(",")[0].split("\\(")[1]);
+                int rightDigit = Integer.parseInt(matcher.group().split(",")[1].split("\\)")[0]);
+                if (leftDigit > 999 || rightDigit > 999 || leftDigit < -999 || rightDigit < -999) {
+                    continue;
+                }
+                total += leftDigit * rightDigit;
+            }
+        }
+        System.out.println("Part One: " + total);
+
     }
 }
