8605f2784a6470cebca7b1630804049fb8aaa604
sander1946 <71840186+sander1946@users.noreply.github.com>
2024-12-03 15:34:55 +0100

    Update dayTwentyThree.java

--- src/main/java/com/advent/of/code/day23/dayTwentyThree.java
+++ src/main/java/com/advent/of/code/day23/dayTwentyThree.java
@@ -7,7 +7,7 @@ import com.advent.of.code.Day;
  */
 public class dayTwentyThree  extends Day {
     /**
-     * Run the solution for day twentyThree
+     * Run the solution for day twenty three
      */
     @Override
     public void run() {
